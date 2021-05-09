7967d850e35128d10f19966e503d75f4c59d0ae0
wolf99 <281700+wolf99@users.noreply.github.com>
2021-05-09 17:39:29 +0100

    Update to intall Ubuntu via winget

--- install-ubuntu.ps1
+++ install-ubuntu.ps1
@@ -1,22 +1,21 @@
-# Installs WSL (v1) Ubuntu from the MS Store
-# Assumes WSL (v1) is already enabled
+# Installs WSL Ubuntu from the MS Store
 #
 # N.B.: Will require a UNIX username and password to be set
 #
-# Detail
+# Requirements
 #
-# I haven't used winget-cli for this as that does not yet have the
-# "versionless" package (it only has wsl-ubuntu-1804)
+# - WSL is enabled and configured
+#   - See enable-wsl-pre-restart.ps1 and enable-wsl-post-restart.ps1
+# - winget-cli (https://github.com/microsoft/winget-cli) - install from Microsoft Store
+#   - See winget-pkgs.ps1
 #
 # Usage
 #
 # PS> .\install-ubuntu.ps1
 
-Write-Host "Downloading wsl-ubuntu from Microsoft Store"
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu -OutFile ~/Ubuntu.appx -UseBasicParsing
+Write-Host "Using WinGet to install Ubuntu"
+winget install Canonical.Ubuntu
 
-Write-Host "Installing wsl-ubuntu package"
-Add-AppxPackage -Path ~/Ubuntu.appx
-
-Write-Host "Running Ubuntu install command"
-ubuntu install
+## No idea if this bit is needed any more
+#Write-Host "Running Ubuntu install command"
+#ubuntu install
