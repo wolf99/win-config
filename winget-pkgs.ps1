d8464af427be0e35e3198891ba2a553fbc09700e
wolf99 <281700+wolf99@users.noreply.github.com>
2021-05-09 17:43:14 +0100

    Refactor paths to vars

--- winget-pkgs.ps1
+++ winget-pkgs.ps1
@@ -15,14 +15,18 @@
 # PS> .\winget-pkgs.ps1
 #
 
+$new_winget_settings "settings.json"
+$winget_settings = "C:\Users\tobym\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+$new_winget_pkgs = "winget-pkgs.json"
+
 Write-Host "Overwriting WinGet default settings with personal settings"
-Copy-Item -Path settings.json -Destination "C:\Users\tobym\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+Copy-Item -Path $new_winget_settings -Destination  $winget_settings
 
 Write-Host "Updating WinGet sources"
 winget source update
 
 Write-Host "Installing packages via WinGet"
-winget import -i .\winget-pkgs.json
+winget import -i $new_winget_pkgs
 
 Write-Host "Installing Code Settings Sync extension in VSCode"
 Write-Host "You should restore settings from your GitHub gist"
